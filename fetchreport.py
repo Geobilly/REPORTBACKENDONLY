1e5b3730df27a64f7005aa15078ed9129f912d7e
Geobilly <102395479+Geobilly@users.noreply.github.com>
2023-11-28 09:30:40 +0000

    Update fetchreport.py

--- fetchreport.py
+++ fetchreport.py
@@ -1,23 +1,36 @@
 from flask import Flask, jsonify
 from flask_restful import Api, Resource
-from flask_cors import CORS  # Import CORS
-import csv
+from flask_cors import CORS
+import mysql.connector
 
 app = Flask(__name__)
 CORS(app)  # Enable CORS for all routes
 api = Api(app)
 
-# CSV file path
-CSV_FILE = 'reportdb.csv'
+# MySQL database configuration
+DB_HOST = '103.195.100.122'
+DB_USER = 'kempsgra_report'
+DB_PASSWORD = 'Billyabban@2000'
+DB_DATABASE = 'kempsgra_reportsystem'  # Change to your actual database name
 
-# Function to load all reports from CSV
+# Establish a connection to the MySQL database
+db_connection = mysql.connector.connect(
+    host=DB_HOST,
+    user=DB_USER,
+    password=DB_PASSWORD,
+    database=DB_DATABASE
+)
+
+# Function to load all reports from the MySQL database
 def load_all_reports():
     try:
-        with open(CSV_FILE, 'r') as file:
-            reader = csv.DictReader(file)
-            reports = list(reader)
+        cursor = db_connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM report")
+        reports = cursor.fetchall()
+        cursor.close()
         return reports
-    except FileNotFoundError:
+    except Exception as e:
+        print(f"Failed to fetch reports: {str(e)}")
         return []
 
 # Resource for handling report fetching
